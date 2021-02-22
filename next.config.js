d11b02b5705dc39cc50edb4ab9393b77013fdb7f
montekaka <josh.chen.jia@gmail.com>
2021-02-21 23:57:08 -0800

    adds config to handle env vars for components

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  /* config options here */
+  env: {
+    RAILS_ENDPOINT: process.env.RAILS_ENDPOINT,
+    REACT_BASE_PATH: process.env.REACT_BASE_PATH,
+    REACT_STRIPE_PUBLIC_KEY: process.env.REACT_STRIPE_PUBLIC_KEY
+  }
+}
