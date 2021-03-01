725456c9001eb633fe7ef4b38ea13f381722699c
montekaka <josh.chen.jia@gmail.com>
2021-02-28 22:46:55 -0800

    adds condition on showing up email us

--- components/EmailUsButton.js
+++ components/EmailUsButton.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 const EmailUsButton = ({subject, email}) => {
-  return (
-    <a className="btn btn-success btn-block lift" href={`mailto:${email}?subject=${subject}`}>Email us your feedback</a>
-  )
+  if(email) {
+    return (
+      <a className="btn btn-success btn-block lift" href={`mailto:${email}?subject=${subject}`}>Email us your feedback</a>
+    )
+  }
+
+  return null;
 }
 
 export default EmailUsButton;
