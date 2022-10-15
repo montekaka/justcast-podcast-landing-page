9f9b46785654e2ff5baace0d8470a1c90926f630
montekaka <josh.chen.jia@gmail.com>
2022-10-15 01:42:51 -0700

    added page to handle user click link to send request to update status

---
+++ pages/member_feeds/[slug]/review.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+import { useRouter } from 'next/router'
+import Layout from '../../../components/private/Layout'
+import { Alert } from 'reactstrap';
+
+const Review = () => {
+  const [message, setMessage] = useState(null);
+
+  const router = useRouter();
+  const { slug, status, token } = router.query;
+  const url = `${process.env.RAILS_ENDPOINT}/v1/private_feeds/${slug}/review?status=${status}&token=${token}`;
+  useEffect(() => {
+    if(slug && status && token) {
+      axios.get(url)
+      .then((res) => {
+        setMessage(res.data.message)
+      })
+      .catch((err) => {
+        setMessage(err.response.data.message)
+      })
+    }
+  }, [slug, status, token]);
+
+  return (
+    <Layout podcast_title="Member feed review">
+      {message && <Alert color="light">
+        <h4 className="alert-heading">Updated</h4>
+        <p>{message}</p>
+      </Alert>}
+    </Layout>    
+  )
+}
+
+export default Review
